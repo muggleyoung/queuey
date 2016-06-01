cda2d7a214dff8e1c5113f1e1fe5bd1434e2e280
Yang Mei <mei.yang@quinyx.com>
2016-06-01 15:08:37 +0200

    fix render in view

--- queuey/views.py
+++ queuey/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from queuey.models import Queue
+from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
