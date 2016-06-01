3f8e421cb2f6be6da963b0ed77691f1552855d13
Edgar Fabricio Santos Rincon <edgar.santos@quinyx.com>
2016-06-01 13:29:05 +0200

    JSX setup

---
+++ frontend/Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function (grunt) {
+   grunt.initConfig({
+      browserify: {
+         dist: {
+            options: {
+               transform: [["babelify", {presets: ["react"]}]]
+            },
+            files: {
+               // if the source file has an extension of es6 then
+               // we change the name of the source file accordingly.
+               // The result file's extension is always .js
+               "./assets/app.js": ["./src/main.jsx"]
+            }
+         }
+      },
+      watch: {
+         scripts: {
+            files: ["./src/*.jsx"],
+            tasks: ["browserify"]
+         }
+      }
+   });
+
+   grunt.loadNpmTasks("grunt-browserify");
+   grunt.loadNpmTasks("grunt-contrib-watch");
+
+   grunt.registerTask("default", ["watch"]);
+   grunt.registerTask("build", ["browserify"]);
+};
